6e69ebc21cc01a347ebd3a35b9b7da1d63bd0a17
yoojin <yoojin@odinsoft.co.kr>
2023-04-04 18:22:21 +0900

    section 4

--- will.sol
+++ will.sol
@@ -29,6 +29,46 @@ contract Will{
         isDead = false;
     }
 
-    
+    // ===: 반드시 서로 같아야 함, ==: 서로 같아야 함
+    // create modifier so the only person who can call the contract is the owner
+    modifier onlyOwner{
+        require(msg.sender == owner);
+        _;
+    }
+ 
+    // create modifier so that we only allocate funds if owners dead
+    modifier mustBeDeceased{
+        require(isDead);
+        _; 
+    }
 
+    //list of family wallets
+    address payable[] familyWallets;
+
+    //iteration iterating - looping through - when we map through we iterate through key store
+    //mapping
+    mapping(address => uint) inheritance;
+
+    // payable 명시한 함수에서만 이더(ether)를 보낼 수 있음
+    // set inheritance for each address
+    function setInheritance(address payable wallet, uint amount) public onlyOwner {
+        // to add wallets to the family wallets
+        familyWallets.push(wallet);
+        inheritance[wallet] = amount;
+
+    }
+
+    // 지급 자동화 기능
+    // pay each family member based on their wallet address
+    function payout() private mustBeDeceased{
+        for(uint i = 0; i < familyWallets.length; i++){
+            familyWallets[i].transfer(inheritance[familyWallets[i]]);
+        }
+    }
+
+    // oracle switch simulation
+    function hasDeceased() public onlyOwner{
+        isDead = true;
+        payout();
+    }
 }
